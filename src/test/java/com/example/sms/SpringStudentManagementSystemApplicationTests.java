23773274743cf9179ae2f9ab18a020c35aa71301
AKnox1234 <90276429+AKnox1234@users.noreply.github.com>
2022-11-26 22:42:46 +0000

    Initial commit

---
+++ src/test/java/com/example/sms/SpringStudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
